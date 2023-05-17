abe48b6a6c48d88153dbb960c905f6496ad629b4
Marco Ramirez <marco.ramirez.a@hotmail.com>
2023-05-17 12:00:46 -0500

    Phase 1 Functions Continue

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun (activity = 'roller-skate') {
+    return (`This Saturday, I want to ${activity}!`)
+}
+const mondayWork = function (task = `go to the office`){
+     return `This Monday, I will ${task}.`
+}
+function wrapAdjective ( idk = `*`){
+    return function (idk2 = `special`){
+        return `You are ${idk}${idk2}${idk}!`
+    }
+}
